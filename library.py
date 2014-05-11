799fb8a411038f83dd4a774559f2e14412613413
Saxon Landers <saxon@ackwell.com.au>
2014-05-11 18:33:26 +1000

    Made decorator for library functions

--- library.py
+++ library.py
@@ -1,13 +1,19 @@
 
+def definition(function):
+	class wrapper:
+		def call(*args, **kwargs):
+			function(*args, **kwargs)
+	return wrapper
+
 class Library(object):
 	def get(self, name):
 		name = '_func_' + name
 		if hasattr(self, name):
 			return getattr(self, name)
 
-	class _func_out:
-		def call(args):
-			print(*args)	
+	@definition
+	def _func_out(args):
+		print(*args)	
 
 	def set(self, name, value):
 		raise SyntaxError() # TODO: more info, can't have them overriding builtins
