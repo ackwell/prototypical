4326be41aa4a7319e4a474fb9e8ee7e2c974710e
Saxon Landers <saxon@ackwell.com.au>
2014-05-24 00:28:56 +1000

    Addig some junk to the lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -8,6 +8,7 @@ import (
 type Lexer struct {
 	src []byte
 
+	char rune
 	pos int
 }
 
@@ -20,18 +21,34 @@ func New(src []byte) Lexer {
 func (l *Lexer) Init(src []byte) {
 	l.src = src
 
+	l.char = ' '
 	l.pos = 0
 }
 
 func (l *Lexer) Next() (tok token.Token) {
+// nextToken:
 	return token.UNKNOWN
 }
 
-// 'Borrowed' from go/scanner.
-func isLetter(ch rune) bool {
-	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch >= 0x80 && unicode.IsLetter(ch)
+func (l *Lexer) ignoreWhitespace() {
+	for isWhitespace(l.char) {
+		l.next()
+	}
 }
 
-func isDigit(ch rune) bool {
-	return '0' <= ch && ch <= '9' || ch >= 0x80 && unicode.IsDigit(ch)
+func (l *Lexer) next(){
+
+}
+
+func isWhitespace(char rune) bool {
+	return char == ' ' || char == '\t' || char == '\n' || char == '\r'
+}
+
+// 'Borrowed' from go/scanner.
+func isLetter(char rune) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_' || char >= 0x80 && unicode.IsLetter(char)
+}
+
+func isDigit(char rune) bool {
+	return '0' <= char && char <= '9' || char >= 0x80 && unicode.IsDigit(char)
 }
